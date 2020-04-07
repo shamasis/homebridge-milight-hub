f43d7fde389acf6d2df34bd65fb0f66bfdb5ad35
Shamasis Bhattacharya <shamasis@getpostman.com>
2020-04-08 00:51:44 +0530

    Smoothened handling of colour in white mode

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const MilightAccessory = require('./lib/accessory');
+// const MilightHubPlatform = require('./lib/platform');
 
 module.exports = function (homebridge) {
   let accessory = MilightAccessory(homebridge);
-  homebridge.registerAccessory(accessory.PLUGIN_NAME, accessory.ACCESSORY_NAME, accessory);
+    // platform = MilightHubPlatform(homebridge);
 
-  // const MilightHubPlatform = require('./lib/platform');
-  // let platform = MilightHubPlatform(homebridge);
+  homebridge.registerAccessory(accessory.PLUGIN_NAME, accessory.ACCESSORY_NAME, accessory);
   // homebridge.registerPlatform(platform.PLUGIN_NAME, platform.PLATFORM_NAME, platform, true);
 };
