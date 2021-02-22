aaf18b2adc6012219415c4edbd18bcf8df3c4a4f
Shamasis Bhattacharya <shamasis@postman.com>
2021-02-22 22:25:41 +0530

    TS WIP

---
+++ src/settings.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    /**
+     * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
+     * @constant
+     */
+    PLATFORM_NAME: 'Milight Hub',
+
+    /**
+     * This must match the name of your plugin as defined the package.json
+     * @constant
+     */
+    PLUGIN_NAME: 'homebridge-milight-hub'
+};
