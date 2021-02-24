661efecd973980dc9dc920a04d0d82e2c6d46f70
Shamasis Bhattacharya <shamasis@postman.com>
2021-02-24 16:54:41 +0530

    Added experimental auto discovery feature

--- src/platform.js
+++ src/platform.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const { PLATFORM_NAME, PLUGIN_NAME } = require('./settings');
 const { MilightPlatformAccessory } = require('./platform-accessory');
 
@@ -28,8 +29,16 @@ class MilightPlatform {
     this.api.on('didFinishLaunching', () => {
       log.debug('Executed didFinishLaunching callback');
       // run the method to discover / register your devices as accessories
-      this.loadDevices();
+      this.loadDevices(config.devices);
       this.pruneDevices();
+
+      if (this.config.autoDiscoverDevices) {
+        log.debug('Attempting to auto discover devices');
+        this.discoverDevices((err) => {
+          log.debug('Executed discoverDevices callback', err);
+        });
+      }
+
     });
   }
 
@@ -49,10 +58,10 @@ class MilightPlatform {
    * Accessories must only be registered once, previously created accessories
    * must not be registered again to prevent "duplicate UUID" errors.
    */
-  loadDevices() {
+  loadDevices(devices, fromAutoDiscovery) {
     const accessoriesToRegister = []; // this array will hold accessories that needs to be appended
 
-    (this.config.accessories || []).forEach((device) => {
+    (devices || []).forEach((device) => {
       
       const uuid = this.api.hap.uuid.generate(MilightPlatformAccessory.getUUIDBaseForDevice(device)),
         existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
@@ -81,7 +90,7 @@ class MilightPlatform {
         // the `context` property can be used to store any data about the accessory you may need
         accessory.context.device = device;
         accessory.context.state = {};
-        accessory.context.autoDiscovered = false;
+        accessory.context.autoDiscovered = Boolean(fromAutoDiscovery);
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
@@ -102,7 +111,7 @@ class MilightPlatform {
    * within config.
    */
   pruneDevices () {
-    const configuredAccessories = this.config.accessories || [],
+    const configuredAccessories = this.config.devices || [],
       loadedAccessories = this.accessories || [];
     
     let surplusAccessories = loadedAccessories.filter((accessory) => {
@@ -119,6 +128,55 @@ class MilightPlatform {
       this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, surplusAccessories);
       this.log.info('Removing existing accessory from cache:', surplusAccessories.map((accessory) => accessory.displayName));
     }
+  }
+
+  /**
+   * Query the hub to load devices
+   */
+  discoverDevices (callback) {
+
+    request({
+      baseUrl: this.config.hub,
+      uri: '/settings',
+      method: 'get',
+      json: true,
+      timeout: 15000
+    }, (err, res, body) => {
+      if (err) { return callback(err); }
+      if (res.statusCode !== 200) { return callback(new Error('Received not OK response from hub')); }
+
+      const devices = Object.entries(body.group_id_aliases || {}).map((entries => ({ // dont ask!
+        displayName: entries[0],
+        deviceAlias: entries[0],
+        remoteType: entries[1][0],
+        deviceId: entries[1][1],
+        deviceGroup: entries[1][2]
+      })));
+
+      this.log.debug(`Discovered ${devices.length} device(s).`)
+      if (devices.length) {
+        this.loadDevices(devices, true);
+      }
+
+      // we now need to remove the previous auto-discovered devices
+      const surplusAccessories = this.accessories.filter((accessory) => {
+        if (!accessory.context.autoDiscovered) {
+          return false;
+        }
+
+        // we mark true to filter this accessory if, it's UUID is not in the discoveredDevicelist
+        return !devices.find((device) => {
+          return this.api.hap.uuid.generate(MilightPlatformAccessory.getUUIDBaseForDevice(device)) === accessory.UUID;
+        });
+      });
+
+      if (surplusAccessories.length) {
+        this.log.info('Removing undiscovered accessory from cache:', surplusAccessories.map((accessory) => accessory.displayName));
+        this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, surplusAccessories);
+      }
+
+      callback(null);
+    })
   }
 }
 
